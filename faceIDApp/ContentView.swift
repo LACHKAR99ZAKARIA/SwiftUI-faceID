ffc825feef5d9d22f0e990d4cd42cc08d3fbe61a
LACHKAR99ZAKARIA <lachkar99zaakria@gmail.com>
2023-06-19 13:26:12 +0100

    Initial Commit

---
+++ faceIDApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  faceIDApp
+//
+//  Created by Zakarai Lachkar on 19/6/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ faceIDApp/faceIDAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  faceIDAppApp.swift
+//  faceIDApp
+//
+//  Created by Zakarai Lachkar on 19/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct faceIDAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
